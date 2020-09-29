738b18865b01af450c370b26dde3939918153ba5
Daniel Firer <firerdaniel@gmail.com>
2020-09-29 09:01:38 +0300

    Refactor bound name resolution

--- src/interpreter.erl
+++ src/interpreter.erl
@@ -14,43 +14,48 @@ interpret(Node) ->
 
 interpret({expressions, Expressions}, Scope) -> 
     lists:foldl(fun(E, {_, S}) -> 
-            interpret_rhs(E, S)
+            interpret(E, S)
         end, {nil, Scope}, Expressions);
 
 interpret({integer, _, Value}, Scope) ->
     {Value, Scope};
 interpret({name, _, Name}, Scope) ->
-    {resolve({name, Name}, Scope#scope.bound_names), Scope};
+    case get(Name, Scope) of
+        {ok, Value} ->
+            {Value, Scope};
+        {error, not_found} ->
+            {{name, Name}, Scope}
+    end;
 
 interpret({{'+', _}, Left, Right}, Scope) ->
-    {LeftValue, Scope1} = interpret_rhs(Left, Scope),
-    {RightValue, Scope2} = interpret_rhs(Right, Scope1),
+    {LeftValue, Scope1} = interpret(Left, Scope),
+    {RightValue, Scope2} = interpret(Right, Scope1),
     {LeftValue + RightValue, Scope2};
 interpret({{'-', _}, Left, Right}, Scope) ->
-    {LeftValue, Scope1} = interpret_rhs(Left, Scope),
-    {RightValue, Scope2} = interpret_rhs(Right, Scope1),
+    {LeftValue, Scope1} = interpret(Left, Scope),
+    {RightValue, Scope2} = interpret(Right, Scope1),
     {LeftValue - RightValue, Scope2};
 interpret({{'*', _}, Left, Right}, Scope) ->
-    {LeftValue, Scope1} = interpret_rhs(Left, Scope),
-    {RightValue, Scope2} = interpret_rhs(Right, Scope1),
+    {LeftValue, Scope1} = interpret(Left, Scope),
+    {RightValue, Scope2} = interpret(Right, Scope1),
     {LeftValue * RightValue, Scope2};
 interpret({{'/', _}, Left, Right}, Scope) ->
-    {LeftValue, Scope1} = interpret_rhs(Left, Scope),
-    {RightValue, Scope2} = interpret_rhs(Right, Scope1),
+    {LeftValue, Scope1} = interpret(Left, Scope),
+    {RightValue, Scope2} = interpret(Right, Scope1),
     {LeftValue / RightValue, Scope2};
 interpret({{'%', _}, Left, Right}, Scope) ->
-    {LeftValue, Scope1} = interpret_rhs(Left, Scope),
-    {RightValue, Scope2} = interpret_rhs(Right, Scope1),
+    {LeftValue, Scope1} = interpret(Left, Scope),
+    {RightValue, Scope2} = interpret(Right, Scope1),
     {LeftValue rem RightValue, Scope2};
 interpret({{'^', _}, Left, Right}, Scope) ->
-    {LeftValue, Scope1} = interpret_rhs(Left, Scope),
-    {RightValue, Scope2} = interpret_rhs(Right, Scope1),
+    {LeftValue, Scope1} = interpret(Left, Scope),
+    {RightValue, Scope2} = interpret(Right, Scope1),
     {math:pow(LeftValue, RightValue), Scope2};
 
 interpret({{'=', _}, Left, Right}, Scope) ->
     {LeftValue, Scope1} = interpret(Left, Scope),
-    {RightValue, #scope{bound_names = BoundNames} = Scope2} = interpret_rhs(Right, Scope1),
-    case resolve(LeftValue, Scope2) of
+    {RightValue, #scope{bound_names = BoundNames} = Scope2} = interpret(Right, Scope1),
+    case LeftValue of
         {name, Name} -> 
             {RightValue, Scope2#scope{bound_names = BoundNames#{Name => RightValue}}};
         _ ->
@@ -61,21 +66,15 @@ interpret({{'=', _}, Left, Right}, Scope) ->
 %% Internal functions
 %%====================================================================
 
-resolve({name, Name}, #scope{parent_scope = Parent, bound_names = BoundNames}) ->
+get(Name, #scope{parent_scope = Parent, bound_names = BoundNames}) ->
     case {maps:is_key(Name, BoundNames), Parent} of
         {true, _} ->
-            maps:get(Name, BoundNames);
+            {ok, maps:get(Name, BoundNames)};
         {false, nil} ->
-            {name, Name};
+            {error, not_found};
         {false, _} ->
-            resolve({name, Name}, Parent)
-    end;
-resolve(Value, _) ->
-    Value.
-
-interpret_rhs(Node, Scope) ->
-    {Value, Scope1} = interpret(Node, Scope),
-    {resolve(Value, Scope1), Scope1}.
+            get(Name, Parent)
+    end.
 
 match(Left, Right, Scope) when Left == Right ->
     {Left, Scope}.
